6597c308efa783492f2a4752ecefb8f34385e9b9
AbedAlMajid Nader AlSaid <abedalmajeedalsaid@gmail.com>
2025-10-03 04:51:26 +0300

    convert workphone workmobile to number some enhancing

--- models/Department.js
+++ models/Department.js
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
 
 const departmentSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  company: { type: String, required: true },
-  manager_id: { type: mongoose.Schema.Types.ObjectId, ref: "Employee" } // top-level manager
+  name: { type: String, required: true ,trim:true, minlength: 2, maxlength: 30 },
+  description: { type: String, trim: true, maxlength: 30 },
+  company: { type: String, required: true,trim:30, maxlength: 30 },
+  manager_id: { type: mongoose.Schema.Types.ObjectId, ref: "Employee" }, // top-level manager
+  timestamps: true,
 });
 
 const Department = mongoose.model("Department", departmentSchema);
