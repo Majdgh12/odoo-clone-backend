76d2d7782d25b28fe801fb4456fa41cdb8dd3f27
abed sleiman <abdelkadersleiman599@gmail.com>
2025-10-16 11:28:26 +0300

    n

--- controllers/project-controller.js
+++ controllers/project-controller.js
@@ -136,14 +136,14 @@ export const deleteProject = async (req, res) => {
     if (!project) return res.status(404).json({ success: false, message: "Project not found" });
 
     // Check authorization: admin or manager of department
-    const user = req.user;
+   /* const user = req.user;
     if (!user) return res.status(401).json({ success: false, message: "Unauthorized" });
     if (
       user.role !== "admin" &&
       !(user.role === "manager" && String(user.department_id) === String(project.department_id))
     ) {
       return res.status(403).json({ success: false, message: "Forbidden - not allowed to delete this project" });
-    }
+    }*/
 
     await project.deleteOne();
 
